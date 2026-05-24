12da2aca3809016d6200f2d3d3df9971c600c731
jirayut-l <jirayut.laorpongphruek@gmail.com>
2026-05-24 10:41:05 +0700

    delete project frontend

--- frontend/src/core/entities/subscription.ts
+++
@@ -1,8 +0,0 @@
-export interface Subscription {
-  id: string;
-  userId: string;
-  accountId: string;
-  name: string;
-  amount: string; // Stored as a string representing a decimal
-  billingCycleDay: number;
-}
